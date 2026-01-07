bf118933a71f21e2bfab917044aa850d2a307395
김남우 <namwoo723@gmail.com>
2026-01-07 17:08:45 +0900

    index.ts complete

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 // TODO 1: EmotionType 유니온 타입을 작성하세요.
 //  감정 종류: happy, sad, angry, anxious, tired, love, sick, cool, excited, meh
 
-export type EmotionType = /* 여기에 직접 작성하세요 */;
+export type EmotionType = "happy" | "sad" | "angry" | "anxious" | "tired" | "love" | "sick" | "cool" | "excited" | "meh"; /* 여기에 직접 작성하세요 */;
 
 // TODO 2: Emotion 인터페이스를 정의하세요
 // 감정 기록 하나는 다음 속성을 포함합니다:
@@ -11,4 +11,8 @@ export type EmotionType = /* 여기에 직접 작성하세요 */;
 // - date: string (YYYY-MM-DD 형식 날짜)
 export interface Emotion {
   // 여기에 직접 작성하세요
+  id: string;
+  emotion: EmotionType;
+  note: string;
+  date: string;
 }
